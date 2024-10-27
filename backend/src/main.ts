91da9acaa07083beee9aa3b1180017c1a8b923d8
saugat-baidwar <saugat.baidwar55@gmail.com>
2024-10-27 13:00:41 +0545

    basic server setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,27 @@
-console.log("hello backend")
+import express, { Request, Response, NextFunction } from "express";
+import { env } from "./utils/config";
+
+const app = express();
+const Port = 4000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json({
+    message: "welcome to book review app",
+    data: null,
+    isSucess: true,
+  });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    message: "internal server error",
+    data: null,
+    isSucess: false,
+  });
+});
+
+app.listen(env.PORT, () => {
+  console.log(`app is listining on port , ${env.PORT}`);
+});
